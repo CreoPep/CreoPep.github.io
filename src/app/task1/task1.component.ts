a8c48a725be008236bd0ba67941dd707c020373a
CreoPep <gc1281003594@gmail.com>
2025-05-04 01:46:13 +0800

    First commit

---
+++ src/app/task1/task1.component.ts
@@ -0,0 +1,40 @@
+import {
+	AfterViewInit,
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	HostListener,
+	OnInit,
+} from '@angular/core';
+
+declare var data: any;
+
+@Component({
+	selector: 'app-task1',
+	templateUrl: './task1.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	styleUrls: ['../shared/task-styles.css'],
+})
+export class Task1Component implements OnInit, AfterViewInit {
+	public taskData = data['Task1'];
+	public selector: any;
+
+	public activeElements: any = {};
+
+	constructor(public changeDetectorRef: ChangeDetectorRef) {
+		changeDetectorRef.detach();
+	}
+
+	ngOnInit(): void {
+		this.changeDetectorRef.detectChanges();
+	}
+	ngAfterViewInit() {
+		const element = document.querySelector('.txt-bg-task');
+		if (element) {
+			(element as HTMLElement).style.setProperty(
+				'--task-number',
+				`"${this.taskData.name}"`
+			);
+		}
+	}
+}
---
+++ src/app/task2/task2.component.ts
@@ -0,0 +1,40 @@
+import {
+	AfterViewInit,
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	HostListener,
+	OnInit,
+} from '@angular/core';
+
+declare var data: any;
+
+@Component({
+	selector: 'app-task2',
+	templateUrl: './task2.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	styleUrls: ['../shared/task-styles.css'],
+})
+export class Task2Component implements OnInit, AfterViewInit {
+	public taskData = data['Task2'];
+	public selector: any;
+
+	public activeElements: any = {};
+
+	constructor(public changeDetectorRef: ChangeDetectorRef) {
+		changeDetectorRef.detach();
+	}
+
+	ngOnInit(): void {
+		this.changeDetectorRef.detectChanges();
+	}
+	ngAfterViewInit() {
+		const element = document.querySelector('.txt-bg-task');
+		if (element) {
+			(element as HTMLElement).style.setProperty(
+				'--task-number',
+				`"${this.taskData.name}"`
+			);
+		}
+	}
+}
